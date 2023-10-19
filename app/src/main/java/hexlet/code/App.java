223611fddadf651d691dffa86c5994a82ce1e080
suhoi <sushilyaz2000@gmail.com>
2023-10-19 22:19:38 +0300

    add menu to App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        System.out.print("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                0 - Exit
+                Your choice:\s""");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        switch (input) {
+            case "1": {
+                System.out.println("Welcome to the Brain Games!");
+                Cli.greeting();
+            }
+        }
     }
 }

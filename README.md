0808dcfed71983f6daaae92cb906ba6979031870
suhoi <sushilyaz2000@gmail.com>
2023-10-20 22:36:53 +0300

    add Calculate.java

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [![Actions Status](https://github.com/sushilyaz/java-project-61/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/sushilyaz/java-project-61/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/b819386f70284d128fff/maintainability)](https://codeclimate.com/github/sushilyaz/java-project-61/maintainability)
 [![asciicast](https://asciinema.org/a/U70SnCYEIy47CEgrB31BLDQV0.svg)](https://asciinema.org/a/U70SnCYEIy47CEgrB31BLDQV0)
+[![asciicast](https://asciinema.org/a/JrrJgIHCedmeda9jQT4hebpKc.svg)](https://asciinema.org/a/JrrJgIHCedmeda9jQT4hebpKc)
---
+++ app/src/main/java/hexlet/code/games/Calculate.java
@@ -0,0 +1,44 @@
+package hexlet.code.games;
+
+import hexlet.code.Randomize;
+
+public class Calculate {
+    public static void calculateGame() {
+        String name = Cli.greeting();
+        System.out.println("""
+                What is the result of the expression?""");
+        boolean check;
+        int num1 = 0;
+        int num2 = 0;
+        int index = 0;
+        String[] arrayOperations = {"+", "-", "*"};
+        String expression = "";
+        String actually = "";
+        String except = "";
+        for (int i = 0; i < 3; i++) {
+            num1 = Randomize.generateNum(1, 1000);
+            num2 = Randomize.generateNum(1, 1000);
+            index = Randomize.generateNum(0, 2);
+            expression = String.valueOf(num1 + arrayOperations[index] + num2);
+            Engine.generateQuastion(expression);
+            switch (arrayOperations[index]) {
+                case "+": {
+                    except = String.valueOf(num1 + num2);
+                    break;
+                }
+                case "*": {
+                    except = String.valueOf(num1 * num2);
+                    break;
+                }
+                case "-": {
+                    except = String.valueOf(num1 - num2);
+                    break;
+                }
+            }
+            actually = Engine.userAnswer();
+            check = Engine.checkAnswer(except, actually);
+            Engine.result(check, except, actually, name);
+        }
+        System.out.println("Congratulations, " + name + "!");
+    }
+}

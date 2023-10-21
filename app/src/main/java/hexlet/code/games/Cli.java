4f16a68cdeabec1bc920914fbbfb16046bc578b1
suhoi <sushilyaz2000@gmail.com>
2023-10-21 12:38:14 +0300

    add "!" to cli xD

--- app/src/main/java/hexlet/code/games/Cli.java
+++ app/src/main/java/hexlet/code/games/Cli.java
@@ -7,7 +7,7 @@ public class Cli {
         System.out.print("May I have your name? ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        System.out.println("Hello, " + input);
+        System.out.println("Hello, " + input + "!");
         return input;
     }
 }
